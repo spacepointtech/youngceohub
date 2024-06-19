4c327c230bbadcc9ba30b06c1df2957d48e7345d
spacepointtecch <spacepointtech@gmail.com>
2024-06-19 11:03:51 +0530

    Submission

---
+++ app/SeconPage.js
@@ -0,0 +1,11 @@
+// app/secondPage.js
+import Layout from './layout';
+import ProblemSection from '@/components/ProblemSection';
+
+export default function SecondPage() {
+  return (
+    <Layout>
+      <ProblemSection />
+    </Layout>
+  );
+}
