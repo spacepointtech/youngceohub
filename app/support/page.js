3dc6627acc7a22a0d192061df9a2cd177d55dee1
spacepointtech <spacepointtech@gmail.com>
2024-06-20 10:25:14 +0530

    update

---
+++ app/support/page.js
@@ -0,0 +1,8 @@
+
+export default function support() {
+  return (
+    <div>
+        <h1>SUPPORT</h1>
+    </div>
+  );
+}
---
+++ app/team/page.js
@@ -0,0 +1,7 @@
+export default function team() {
+    return (
+      <div>
+          <h1>Our Team</h1>
+      </div>
+    );
+  }
