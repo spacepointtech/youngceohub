6bbc88625eca985508d70556028960a77f831055
spacepointtech <spacepointtech@gmail.com>
2024-06-22 18:52:46 +0530

    Updated

--- components/ThirdSection.js
+++ components/ThirdSection.js
@@ -1,36 +1,4 @@
-
-// 'use client'; // for client side rendering
-
-// import Image from 'next/image';
-
-// export default function ThirdSection() {
-//   return (
-//     <div className="min-h-screen bg-black text-white py-20 px-4 md:px-0 font-hammersmith-one">
-//       <div className="grid md:grid-cols-2 gap-8 max-w-6xl mx-auto">
-//         <div className="flex flex-col justify-center space-y-4">
-//           <h2 className="text-5xl font-bold">WE’RE SOLELY FOR THE SMALL AND INDEPENDENT ARTISTS</h2>
-//           <p className="text-2xl">
-//             YCE is your hub for discovering new artists. Hear under-the-radar artists here first.
-//           </p>
-//         </div>
-//         <div className="flex justify-center md:justify-end space-x-4 mt-4 md:mt-0">
-//           <div className="bg-gray-700 p-4 rounded">
-//             <Image src="/images/artist1.jpeg" alt="Artist 1" width={200} height={100} className="h-auto rounded" />
-//           </div>
-//           {/* <div className="bg-gray-700 p-4 rounded">
-//             <Image src="/images/artist2.jpeg" alt="Artist 2" width={200} height={100} className="h-auto rounded" />
-//           </div> */}
-//           <div className="bg-gray-700 p-4 rounded">
-//             <Image src="/images/artist3.jpeg" alt="Artist 3" width={200} height={100} className="h-auto rounded" />
-//           </div>
-//           {/* <div className="bg-gray-700 p-4 rounded">
-//             <Image src="/images/artist4.jpeg" alt="Artist 4" width={100} height={100} className="h-auto rounded" />
-//           </div> */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+// ThirdSection.js 
 
 'use client'; // for client side rendering
 
